a05bbc3a195f0d63ae703c2adc70e7a3c82b78e9
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-08 01:12:33 +0530

    DB to CSV project

---
+++ spring-batch/springbatch-db-to-csv/src/main/java/com/project/batch/SpringbatchDbToCsvApplication.java
@@ -0,0 +1,12 @@
+package com.project.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchDbToCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchDbToCsvApplication.class, args);
+	}
+}
---
+++ spring-batch/springbatch-db-to-csv/src/test/java/com/project/batch/SpringbatchDbToCsvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.batch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbatchDbToCsvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
