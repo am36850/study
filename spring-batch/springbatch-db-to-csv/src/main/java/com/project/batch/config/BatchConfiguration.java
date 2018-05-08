594a39b295752d30b30bd6d9d1e273b8a627d3a1
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-08 22:51:14 +0530

    Writing file to external File location.

--- spring-batch/springbatch-db-to-csv/src/main/java/com/project/batch/config/BatchConfiguration.java
+++ spring-batch/springbatch-db-to-csv/src/main/java/com/project/batch/config/BatchConfiguration.java
@@ -1,6 +1,7 @@
 package com.project.batch.config;
 
-import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
@@ -11,11 +12,17 @@ import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.batch.item.file.transform.FieldExtractor;
 import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.data.domain.Sort.Direction;
 import com.project.batch.dto.PersonDto;
 import com.project.batch.listener.DurationCaptureListener;
 import com.project.batch.model.Person;
@@ -41,29 +48,56 @@ public class BatchConfiguration {
   @Autowired
   private PersonItemProcessor itemProcessor;
 
+  @Value("${output.folder.path}")
+  String outputFolderPath;
+
   @Autowired
-  DataSource datasource;
+  ApplicationContext context;
 
   @Bean
   public RepositoryItemReader<Person> reader() {
     return new RepositoryItemReaderBuilder<Person>().name("PersonItemReader")
-        .repository(personRepository).methodName("findAll").build();
+        .repository(personRepository).methodName("findAll").sorts(getSortOrder()).build();
 
+  }
+
+  private Map<String, Direction> getSortOrder() {
+    Map<String, Direction> sortOrder = new HashMap<>();
+    sortOrder.put("personId", Direction.ASC);
+    return sortOrder;
   }
 
   @Bean
   public FlatFileItemWriter<PersonDto> writer() {
-    return new FlatFileItemWriterBuilder<PersonDto>().lineAggregator(getLineAggregator())
-        .forceSync(Boolean.TRUE).resource(new ClassPathResource("person.csv")).build();
+    return new FlatFileItemWriterBuilder<PersonDto>().name("CSVFileWriter")
+        .lineAggregator(getLineAggregator()).forceSync(Boolean.TRUE)
+        .resource(new FileSystemResource(getOutputFile())).build();
+  }
+
+  private String getOutputFile() {
+    return outputFolderPath + generateFileName();
+  }
+
+  private String generateFileName() {
+    return "ExportUsers_" + System.currentTimeMillis() + ".csv";
   }
 
   private LineAggregator<PersonDto> getLineAggregator() {
-    return null;
+    DelimitedLineAggregator<PersonDto> lineAggregator = new DelimitedLineAggregator<>();
+    lineAggregator.setDelimiter(",");
+    lineAggregator.setFieldExtractor(getFieldExtractor());
+    return lineAggregator;
+  }
+
+  private FieldExtractor<PersonDto> getFieldExtractor() {
+    BeanWrapperFieldExtractor<PersonDto> fieldExtractor = new BeanWrapperFieldExtractor<>();
+    fieldExtractor.setNames(new String[] {"firstName", "lastName", "age", "email"});
+    return fieldExtractor;
   }
 
   @Bean
   public Step step1() {
-    return stepBuilderFactory.get("step1").<Person,PersonDto>chunk(10).reader(reader())
+    return stepBuilderFactory.get("step1").<Person, PersonDto>chunk(10).reader(reader())
         .processor(itemProcessor).writer(writer()).build();
   }
 
