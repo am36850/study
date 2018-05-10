4ade44da6dd1ddb65b38be4548a7e52becc9b45c
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-10 08:13:17 +0530

    Initial Commit

---
+++ spring-batch/springbatch-xml-to-db/src/main/java/com/project/batch/SpringbatchXmlToDbApplication.java
@@ -0,0 +1,12 @@
+package com.project.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchXmlToDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchXmlToDbApplication.class, args);
+	}
+}
