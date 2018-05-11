3caeb4aa7a980a2d7ae6f46bfc5961098bd84cdd
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-11 20:31:03 +0530

    initial commit

---
+++ spring-batch/springbatch-multiresource-reader/src/main/java/com/project/batch/springbatchmultiresourcereader/SpringbatchMultiresourceReaderApplication.java
@@ -0,0 +1,12 @@
+package com.project.batch.springbatchmultiresourcereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchMultiresourceReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchMultiresourceReaderApplication.class, args);
+	}
+}
