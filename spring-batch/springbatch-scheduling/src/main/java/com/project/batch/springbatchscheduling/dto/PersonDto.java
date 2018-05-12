23fd1887b1e42a7ae82fd13bf253fe2706f9af29
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-12 17:41:07 +0530

    initial commit

---
+++ spring-batch/springbatch-scheduling/src/main/java/com/project/batch/springbatchscheduling/dto/PersonDto.java
@@ -0,0 +1,53 @@
+package com.project.batch.springbatchscheduling.dto;
+
+public class PersonDto {
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String age;
+	
+	public PersonDto() {
+		super();
+	}	
+	
+	public PersonDto(String firstName, String lastName, String email, String age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", age=" + age + "]";
+	}	
+	
+	
+}
