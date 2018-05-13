2cad2aaea5f6f0992878f4278abb69c7b2c6fb0f
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-13 10:02:39 +0530

    Revert "Revert "initial commit""
    This reverts commit d0b0cb3

---
+++ spring-batch/springbatch-tasklet/src/main/java/com/project/batch/springbatchtasklet/SpringbatchTaskletApplication.java
@@ -0,0 +1,12 @@
+package com.project.batch.springbatchtasklet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchTaskletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchTaskletApplication.class, args);
+	}
+}
