39b2c17033a1f82c8543f490a9bc1fa611278502
Anant Mane <anant.mane.freelance@gmail.com>
2018-05-15 22:49:42 +0530

    Initial Commit

---
+++ spring-batch/spring-parallel-processing/src/main/java/com/project/batch/springparallelprocessing/SpringParallelProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.project.batch.springparallelprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParallelProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringParallelProcessingApplication.class, args);
+	}
+}
---
+++ spring-batch/spring-parallel-processing/src/test/java/com/project/batch/springparallelprocessing/SpringParallelProcessingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.batch.springparallelprocessing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringParallelProcessingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
