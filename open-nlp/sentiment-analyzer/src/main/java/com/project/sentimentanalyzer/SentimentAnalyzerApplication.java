b09e3d0bc957478703e4dff4bddef50d19f875df
Anant Mane <anant.mane.freelance@gmail.com>
2018-06-16 13:55:59 +0530

    initial

---
+++ open-nlp/sentiment-analyzer/src/main/java/com/project/sentimentanalyzer/SentimentAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.project.sentimentanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalyzerApplication.class, args);
+	}
+}
