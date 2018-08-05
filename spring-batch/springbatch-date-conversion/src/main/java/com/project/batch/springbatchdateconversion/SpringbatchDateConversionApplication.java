a5d4dae80043e1acc11bb0170f47d08c77cc6607
anmane <anmane@mediaocean.com>
2018-08-05 17:23:57 +0530

    initial commit

---
+++ spring-batch/springbatch-date-conversion/src/main/java/com/project/batch/springbatchdateconversion/SpringbatchDateConversionApplication.java
@@ -0,0 +1,12 @@
+package com.project.batch.springbatchdateconversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchDateConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchDateConversionApplication.class, args);
+	}
+}
