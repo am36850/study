fa7a799e8fe7f01f5f14caa8ee96daf5607c72bd
anmane <anmane@mediaocean.com>
2018-08-26 00:51:33 +0530

    server to client communication without message from client

--- websocket/ChatApplication/src/main/java/com/study/websocket/chat/controller/ChatController.java
+++ websocket/ChatApplication/src/main/java/com/study/websocket/chat/controller/ChatController.java
@@ -4,15 +4,27 @@ import com.study.websocket.chat.dto.MessageDto;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@EnableScheduling
 public class ChatController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatController.class);
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    public ChatController(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
@@ -21,6 +33,17 @@ public class ChatController {
         MessageDto messageDto = message.getPayload();
         messageDto.setReply("Request Processed successfully");
         LOGGER.info("Sending Response :{}",messageDto);
+        return messageDto;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void sendMessage(){
+        simpMessagingTemplate.convertAndSend("/topic/messages",getMessageDto());
+    }
+
+    private Object getMessageDto() {
+        MessageDto messageDto = new MessageDto();
+        messageDto.setFrom("Anant");
         return messageDto;
     }
 }
